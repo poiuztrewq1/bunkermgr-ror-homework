81926b23436c32c5fce41f53d56846bf429b2053
poiuztrewq1 <bucsy.beni@gmail.com>
2020-11-02 02:41:47 +0100

    Computational entries added

--- app/models/bunker.rb
+++ app/models/bunker.rb
@@ -2,4 +2,20 @@ class Bunker < ApplicationRecord
   validates :capacity, numericality: true
   has_many :inventory_items, dependent: :destroy
   has_and_belongs_to_many :users
+
+
+  def sum_stocked_calories
+    inventory_items.map {|i| i.sum_calories}.sum
+  end
+
+  def estimated_stocks
+    # estimation: 2000 kcal per person per day
+    sum_stocked_calories / 2000
+  end
+
+  def next_expiring_item
+    now = Time.new.to_date
+    inventory_items.sort{ |a| a.exp_date.mjd - now.mjd }.first
+  end
+
 end
--- app/models/inventory_item.rb
+++ app/models/inventory_item.rb
@@ -1,4 +1,25 @@
 class InventoryItem < ApplicationRecord
   belongs_to :bunker
   validates :quantity, numericality: true
+
+  def sum_calories
+    nutrition_per_unit * quantity
+  end
+
+  def days_til_exp
+    exp_date.mjd - Time.new.to_date.mjd
+  end
+
+  def expiration_class
+    days = days_til_exp
+    case
+    when days > 30
+      'text-success'
+    when days <= 30 && days > 0
+      'text-warning'
+    when days <= 0
+      'text-danger'
+    end
+  end
+
 end
